7f09ad853d185cf9a7e99f69af0deab8766debd2
LuizF0617 <luizfelipekremerdias@gmail.com>
2025-10-01 22:36:11 +0000

    sitefinal

---
+++ public/public/cardapio.php
@@ -0,0 +1,55 @@
+<?php require_once("./includes/page-top.php") ?>
+
+<div id="page-content">
+    <div class="container" style="padding: 20px;">
+        <div class="row">
+            <div class="col">
+                <div class="card bg-light">
+                    <div class="card-header bg-danger text-light">
+                        Hamburger Especial
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <img src="img/dacasa.png" height="300px">
+                                <div class="col card-text">Delicioso sanduíche com 1 hambúrguer angus, salada, molho barbecue, calabresa, frango e queijo.</div>
+                                <hr />
+                                <div class="text-end">
+                                    <a href="#" class="btn btn-danger">Comprar</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card bg-light">
+                    <div class="card-header bg-danger text-light">
+                        Hamburger da Casa
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <img src="img/especial.jpg" height="300px">
+                                <div class="col card-text">Delicioso sanduíche com 1 hambúrguer angus, salada, molho barbecue, cebola, e queijo cheddar.</div>
+                                <hr />
+                                <div class="text-end">
+                                    <a href="#" class="btn btn-danger">Comprar</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+                <div id="page-content">
+
+                </div>
+<?php require_once("./includes/page-button.php") ?>
+
